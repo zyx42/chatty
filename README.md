0c80484744ea8729ecd82e5055cd9701d2b2f6d8
zyx42 <zyx42@dismail.de>
2021-02-06 00:43:23 +0200

    add WebSocket Message Broker configuration and respective utils

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chatty
+**An easy way to contact your fellow chatterboxes**
+
+This is a simple web application which provides very simple chat service.
+
+#### How to run
+In the root directory there is a script called "chatty.sh".
+First, you need to make it executable by running "chmod +x chatty.sh" from the same directory and then just run it
+with "./chatty.sh" when your terminal is open from the same directory.
+
+#### Important endpoints
+
+- http://localhost:8080 - web-ui for Chatty
---
+++ src/main/java/xyz/chatty/chatty/config/WebSocketMessageBrokerConfig.java
@@ -0,0 +1,32 @@
+package xyz.chatty.chatty.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import xyz.chatty.chatty.util.UserInterceptor;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/broadcast").withSockJS().setHeartbeatTime(60_000);
+        registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new UserInterceptor());
+    }
+}
